aa53bef6128b7ddc964792eade85979fca257db7
leestana01 <leestana01@naver.com>
2024-01-20 01:56:30 +0900

    [INIT] Spring Initializr

---
+++ src/main/java/com/likelion/welcomekit/WelcomekitApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.welcomekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomekitApplication.class, args);
+	}
+
+}
